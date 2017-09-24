410fc44ab4cda3ca06df9f9e1e7adcbac7b5eaf3
Robert van der Tuuk <201292@live.nl>
2017-09-24 06:59:14 +0200

    just playing around a little.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#include <pulse/simple.h>
+#include <pulse/error.h>
+
+const int buffsize = 1024;
+
 int main(int argc, char **argv)
 {
     printf("this is just an initial start\r\n");
